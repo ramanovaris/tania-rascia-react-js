a3d28d537e49b821f83ba6706ac02c6d656c686f
Rama Novaris Ayyubi Pratama <ramaanovariss@gmail.com>
2023-01-20 09:10:23 +0800

    Finish Create React App

--- create-react-app/react-tutorial/src/index.js
+++ create-react-app/react-tutorial/src/index.js
@@ -1,3 +1,16 @@
 import React from 'react'
-import { ReactDOM } from 'react-dom'
+import ReactDOM from 'react-dom'
 import './index.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div
+        className='App'>
+        <h1>Hallo, React!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
