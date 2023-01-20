abc2797929d4581a02e9db9e89790596e71fbb89
Rama Novaris Ayyubi Pratama <ramaanovariss@gmail.com>
2023-01-20 11:42:47 +0800

    mencoba mengirim data dengan Props

--- create-react-app/react-tutorial/src/App.js
+++ create-react-app/react-tutorial/src/App.js
@@ -3,10 +3,29 @@ import Table from './Table'
 
 class App extends Component {
   render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
+
     return (
       <div
         className='container'>
-        <Table />
+        <Table charactersData={characters} />
       </div>
     )
   }
